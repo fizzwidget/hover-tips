ec03398fb65700eef377c30e4690cf645b79d296
Gazmik Fizzwidget <gazmik@fizzwidget.com>
2025-01-19 21:54:42 -0800

    rearchitecture - table of functions to tooltip handlers - new standard GFW addon structure

--- HoverTips.lua
+++ HoverTips.lua
@@ -1,121 +1,169 @@
+------------------------------------------------------
+-- Addon loading & shared infrastructure
+------------------------------------------------------
+local addonName, T = ...
+_G[addonName] = T
+
+T.Title = C_AddOns.GetAddOnMetadata(addonName, "Title")
+T.Version = C_AddOns.GetAddOnMetadata(addonName, "Version")
+
 
 local linkTypes = {
-	item = true,
-	enchant = true, -- any crafting recipe
-	spell = true,
-	quest = true,
-	unit = true, -- used in combat log
-	talent = true,
-	achievement = true,
-	glyph = true, -- no longer usable since glyph system is gone?
-	instancelock = true, -- raidinfo window
-	currency = true,
-	-- BNplayer = true, -- disabled, needs custom handling that's broken now
-	keystone = true, -- untested as of 11.0
-	battlepet = true, -- semi custom handling from BattlePet UI
-	-- perksactivity = true, -- TODO needs custom tooltip handling
-	-- curio = true, -- TODO war within delve buddy stuff
-	-- trade = true, -- TODO link your whole profession
-	-- api = true, -- TODO has to do with /api command for API help
-	-- battlePetAbil = true, -- TODO probably needs similar to battlepet
-	-- dungeonScore = true, -- TODO mythic+ score has a tooltip
-	-- outfit = true, -- TODO transmog outfit
-	-- talentbuild = true, -- TODO whole talent builds has a useful tooltip?
+    item = true,
+    enchant = true, -- any crafting recipe
+    spell = true,
+    quest = true,
+    unit = true, -- used in combat log
+    talent = true,
+    achievement = true,
+    glyph = true, -- no longer usable since glyph system is gone?
+    instancelock = true, -- raidinfo window
+    currency = true,
+    -- BNplayer = true, -- disabled, needs custom handling that's broken now
+    keystone = true, -- untested as of 11.0
+    battlepet = true, -- semi custom handling from BattlePet UI
+    -- perksactivity = true, -- TODO needs custom tooltip handling
+    -- curio = true, -- TODO war within delve buddy stuff
+    -- trade = true, -- TODO link your whole profession
+    -- api = true, -- TODO has to do with /api command for API help
+    -- battlePetAbil = true, -- TODO probably needs similar to battlepet
+    -- dungeonScore = true, -- TODO mythic+ score has a tooltip
+    -- outfit = true, -- TODO transmog outfit
+    -- talentbuild = true, -- TODO whole talent builds has a useful tooltip?
  }
 
-local function ShowJournalTooltip(frame, linkContent)
-	local jtype, id, difficulty = strsplit(":", linkContent);
-	jtype = tonumber(jtype);
-	id = tonumber(id);
-	difficulty = tonumber(difficulty);
-	local instanceID, encounterID, sectionID, tierIndex = EJ_HandleLinkPath(jtype, id);	
-	if (instanceID == nil and encounterID == nil and sectionID == nil) then
-		-- we can get this for dungeon tier links which appear when you level up
-		-- but there's nothing sensible to put in a tooltip for them
-		return;
-	end
-	
-	EJ_SetDifficulty(difficulty);
-	
-	local C = HIGHLIGHT_FONT_COLOR; 
-	if (encounterID == nil) then
-		local name, description = EJ_GetInstanceInfo(instanceID);
-		
-		GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT");
-		GameTooltip:SetText(name, C.r, C.g, C.b);
-		GameTooltip:AddLine(description, nil, nil, nil, true);
-		GameTooltip:Show();
+------------------------------------------------------
+ -- Link handler functions
+ ------------------------------------------------------
 
-	elseif (sectionID == nil) then
-		local instance = EJ_GetInstanceInfo(instanceID);
-		local name, description = EJ_GetEncounterInfo(encounterID);
-		
-		GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT");
-		GameTooltip:SetText(name, C.r, C.g, C.b);
-		GameTooltip:AddLine(instance, C.r, C.g, C.b);
-		GameTooltip:AddLine(description, nil, nil, nil, true);
-		GameTooltip:Show();
-	
-	else
-		local instance = EJ_GetInstanceInfo(instanceID);
-		local encounter = EJ_GetEncounterInfo(encounterID);
-		local name, description = EJ_GetSectionInfo(sectionID);
-		
-		GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT");
-		GameTooltip:SetText(name, C.r, C.g, C.b);
-		GameTooltip:AddLine(encounter .." - ".. instance, C.r, C.g, C.b);
-		GameTooltip:AddLine(description, nil, nil, nil, true);
-		GameTooltip:Show();
-		
-	end
-		
+function T:ShowDefaultTooltip(link)
+    local anchorFrame = self
+    GameTooltip:SetOwner(anchorFrame, "ANCHOR_TOPLEFT")
+    GameTooltip:SetHyperlink(link)
+    GameTooltip:Show()
 end
 
-local function ShowBattlePetTooltip(frame, linkContent)
-	GameTooltip:SetOwner(frame, "ANCHOR_TOPRIGHT")
-	local petInfo = {}
-	for _, string in pairs({strsplit(":", linkContent)}) do
-		tinsert(petInfo, tonumber(string))
-	end
-	BattlePetToolTip_Show(unpack(petInfo))
+function T:ShowJournalTooltip(link)
+    local anchorFrame = self
+    local _, jtype, id, difficulty = strsplit(":", link)
+    jtype = tonumber(jtype)
+    id = tonumber(id)
+    difficulty = tonumber(difficulty)
+    local instanceID, encounterID, sectionID, tierIndex = EJ_HandleLinkPath(jtype, id)
+    if (instanceID == nil and encounterID == nil and sectionID == nil) then
+        -- we can get this for dungeon tier links which appear when you level up
+        -- but there's nothing sensible to put in a tooltip for them
+        return
+    end
+    
+    EJ_SetDifficulty(difficulty)
+    local C = HIGHLIGHT_FONT_COLOR
+    if (encounterID == nil) then
+        local name, description = EJ_GetInstanceInfo(instanceID)
+        GameTooltip:SetOwner(anchorFrame, "ANCHOR_TOPLEFT")
+        GameTooltip:SetText(name, C.r, C.g, C.b)
+        GameTooltip:AddLine(description, nil, nil, nil, true)
+        GameTooltip:Show()
+    elseif (sectionID == nil) then
+        local instance = EJ_GetInstanceInfo(instanceID)
+        local name, description = EJ_GetEncounterInfo(encounterID)
+        GameTooltip:SetOwner(anchorFrame, "ANCHOR_TOPLEFT")
+        GameTooltip:SetText(name, C.r, C.g, C.b)
+        GameTooltip:AddLine(instance, C.r, C.g, C.b)
+        GameTooltip:AddLine(description, nil, nil, nil, true)
+        GameTooltip:Show()
+    else
+        local instance = EJ_GetInstanceInfo(instanceID)
+        local encounter = EJ_GetEncounterInfo(encounterID)
+        local name, description = EJ_GetSectionInfo(sectionID)
+        GameTooltip:SetOwner(anchorFrame, "ANCHOR_TOPLEFT")
+        GameTooltip:SetText(name, C.r, C.g, C.b)
+        GameTooltip:AddLine(encounter .." - ".. instance, C.r, C.g, C.b)
+        GameTooltip:AddLine(description, nil, nil, nil, true)
+        GameTooltip:Show()
+    end
+        
 end
 
-local function OnHyperlinkEnter(frame, link, ...)
-	local linkType, linkContent = link:match("^([^:]+):(.+)")
-	if (linkType) then
-		if (linkType == FDA_LINK_TYPE and FDA_ShowCurrentArtifactTooltipForRace) then
-			local raceIndex = tonumber(linkContent);
-			if (raceIndex) then
-				GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT")
-				FDA_ShowCurrentArtifactTooltipForRace(GameTooltip, raceIndex);
-			end
-		elseif (linkType == FDA_SOLVE_LINK_TYPE and FDA_ShowSolveTooltipForRace) then
-			local raceIndex = tonumber(linkContent);
-			if (raceIndex) then
-				GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT")
-				FDA_ShowSolveTooltipForRace(raceIndex);
-			end
-		elseif (linkType == "journal") then
-			ShowJournalTooltip(frame, linkContent);
-		elseif (linkType == "battlepet") then
-			ShowBattlePetTooltip(frame, linkContent)
-		elseif linkTypes[linkType] then
-			GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT")
-			GameTooltip:SetHyperlink(link)
-			GameTooltip:Show()
-		end
-	end
+function T:ShowBattlePetTooltip(link)
+    local anchorFrame = self
+    GameTooltip:SetOwner(anchorFrame, "ANCHOR_TOPRIGHT")
 
+    local petInfo = {select(2, strsplit(":", link))} -- skip link type
+    for i, element in pairs(petInfo) do 
+       petInfo[i] = tonumber(element)
+    end
+    BattlePetToolTip_Show(unpack(petInfo))
 end
 
-local function OnHyperlinkLeave(frame, ...)
-	GameTooltip:Hide()
-	BattlePetTooltip:Hide()
+-- TODO not hooked up yet, fix when modernizing DiggerAid
+function T:ShowDiggerAidTooltip(link)
+    local linkType, linkContent = link:match("^([^:]+):(.+)")
+    if (linkType == FDA_LINK_TYPE and FDA_ShowCurrentArtifactTooltipForRace) then
+        local raceIndex = tonumber(linkContent)
+        if (raceIndex) then
+            GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT")
+            FDA_ShowCurrentArtifactTooltipForRace(GameTooltip, raceIndex)
+        end
+    elseif (linkType == FDA_SOLVE_LINK_TYPE and FDA_ShowSolveTooltipForRace) then
+        local raceIndex = tonumber(linkContent)
+        if (raceIndex) then
+            GameTooltip:SetOwner(frame, "ANCHOR_TOPLEFT")
+            FDA_ShowSolveTooltipForRace(raceIndex)
+        end
+    end
+end
+
+------------------------------------------------------
+ -- Link handler registry
+ ------------------------------------------------------
+ T.LinkHandlers = {
+    item = T.ShowDefaultTooltip,
+    enchant = T.ShowDefaultTooltip, -- any crafting recipe
+    spell = T.ShowDefaultTooltip,
+    quest = T.ShowDefaultTooltip,
+    unit = T.ShowDefaultTooltip, -- used in combat log
+    talent = T.ShowDefaultTooltip,
+    achievement = T.ShowDefaultTooltip,
+    glyph = T.ShowDefaultTooltip, -- no longer usable since glyph system is gone?
+    instancelock = T.ShowDefaultTooltip, -- raidinfo window
+    currency = T.ShowDefaultTooltip,
+    keystone = T.ShowDefaultTooltip, -- untested as of 11.0
+    
+    journal = T.ShowJournalTooltip,
+    battlepet = T.ShowBattlePetTooltip, -- semi custom handling from BattlePet UI
+    
+    -- BNplayer = true, -- disabled, needs custom handling that's broken now
+    -- player = true, -- disabled, needs custom handling that's broken now
+    -- perksactivity = true, -- TODO needs custom tooltip handling
+    -- curio = true, -- TODO war within delve buddy stuff
+    -- trade = true, -- TODO link your whole profession
+    -- api = true, -- TODO has to do with /api command for API help
+    -- battlePetAbil = true, -- TODO probably needs similar to battlepet
+    -- dungeonScore = true, -- TODO mythic+ score has a tooltip
+    -- outfit = true, -- TODO transmog outfit
+    -- talentbuild = true, -- TODO whole talent builds has a useful tooltip?
+
+}
+
+function T:OnHyperlinkEnter(link)
+    local anchorFrame = self -- when hooked this will be the moused-over frame
+    local linkType = strsplit(":", link) -- first element only
+    
+    handler = T.LinkHandlers[linkType]
+    if handler == nil then return; end
+    
+    handler(anchorFrame, link)
+end
+
+function T:OnHyperlinkLeave(...)
+    GameTooltip:Hide()
+    BattlePetTooltip:Hide()
 end
 
 local _G = getfenv(0)
 for i=1, NUM_CHAT_WINDOWS do
-	local frame = _G["ChatFrame"..i]
-	frame:HookScript("OnHyperlinkEnter", OnHyperlinkEnter)
-	frame:HookScript("OnHyperlinkLeave", OnHyperlinkLeave)
+    local frame = _G["ChatFrame"..i]
+    frame:HookScript("OnHyperlinkEnter", T.OnHyperlinkEnter)
+    frame:HookScript("OnHyperlinkLeave", T.OnHyperlinkLeave)
 end
